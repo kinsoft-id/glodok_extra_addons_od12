48211a126abe17a886e1ccfd0e73ac888b7c2eb1
Kikin Kusumah <kinsoft.indonesia@gmail.com>
2022-09-17 13:10:01 +0700

    -

--- kin_sme_customize/models/sale_order_line.py
+++ kin_sme_customize/models/sale_order_line.py
@@ -8,6 +8,7 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     margin = fields.Float(compute='_compute_margin', inverse="_inverse_margin", readonly=False, string='Margin', store=True)
+    margin_subtotal = fields.Float(compute='_compute_margin_subtotal', inverse="_inverse_margin_subtotal", readonly=False, string='Margin Subtotal', store=True)
     
     order_ref = fields.Char('Customer Reference', related='order_id.client_order_ref')
     customer_id = fields.Many2one('res.partner', related='order_id.partner_id')
@@ -20,7 +21,17 @@ class SaleOrderLine(models.Model):
             fstd_price = '{0:,.2f}'.format(std_price)
             record['margin'] = record.price_unit - std_price
 
+    @api.depends('margin')
+    def _compute_margin_subtotal(self):
+        for record in self:
+            record['margin_subtotal'] = record.margin * record.product_uom_qty
+
     def _inverse_margin(self):
+        for record in self:
+            if not record.margin:
+                continue
+
+    def _inverse_margin_subtotal(self):
         for record in self:
             if not record.margin:
                 continue
